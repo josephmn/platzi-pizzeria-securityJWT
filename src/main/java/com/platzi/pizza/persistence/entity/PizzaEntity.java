2a8a84431e1157363d05ab15bde1df0f037d005d
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-04-26 13:15:37 -0500

    Auditoria para user en pizzas

--- src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -2,16 +2,25 @@ package com.platzi.pizza.persistence.entity;
 
 import com.platzi.pizza.persistence.audit.AuditPizzaListener;
 import com.platzi.pizza.persistence.audit.AuditableEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "pizza")
-//@EntityListeners({AuditingEntityListener.class, AuditPizzaListener.class})
-@EntityListeners({AuditingEntityListener.class})
+@EntityListeners({AuditingEntityListener.class, AuditPizzaListener.class})
+//@EntityListeners({AuditingEntityListener.class})
 @Getter
 @Setter
 @NoArgsConstructor
